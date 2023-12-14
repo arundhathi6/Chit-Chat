3b26027187d8adb10ce4915e50cd17b972383584
Arundhathi R Menon <43034600+arundhathi6@users.noreply.github.com>
2023-12-14 13:18:19 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 
 const connect = ()=>{
     //process.env.MONGODB_URL
-    return mongoose.connect("mongodb+srv://arundhathi:arundhathi009@cluster0.mar6i.mongodb.net/chattingApp?retryWrites=true&w=majority");
+    return mongoose.connect("mongodb+srv://arundhathi:Arundhathi009@cluster0.mar6i.mongodb.net/chattingApp?retryWrites=true&w=majority");
 }
 module.exports=connect;
